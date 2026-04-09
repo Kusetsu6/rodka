98cebf015dc8371dca3a83ca438f18d3e989e803
Lyceum <lukantatarenkov@gmail.com>
2026-04-09 09:46:21 +0300

    rodka

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup,KeyboardButton
 
-import asyncio
 
 
 API_TOKEN = "8756157675:AAHO6Nk1hJUtNvs_y3-LF9EgFjhjmlnSK34"
@@ -12,21 +11,31 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(Command("start"))
-async def start_command(message: Message):
-    await message.answer("Привіт! я телеграм бот. напиши команду /help, щоб дізнатися, що я вмію!")
-
-
-
-@dp.message(Command("help"))
-async def help_command(message: Message):
-    help_text = (
-        "Доступні команди:\n"
-        "/start - Привітатися з ботом\n"
-        "/help - Показати цю довідку\n\n"
-        "Я поки буду для тебе не настільки корисним, але може в майбутньому я стану розумніше"
+@dp.message(Command("menu"))
+async def show_menu(message: Message):
+    keyboard = ReplyKeyboardMarkup(keyboard=[
+        KeyboardButton(text ="Привіт"),
+        KeyboardButton(text = "Як справи?"),
+        KeyboardButton(text = "Анекдот")
+    ],
+        resize_keyboard=True
     )
-    await message.answer(help_text)
+    await message.answer("Вибери опцію:")
+
+
+
+@dp.message()
+async def handle_message(message: Message):
+    text = message.text
+    if text == "Привіт":
+        await message.answer("Привіт-привіт")
+    elif text == "Як справи?":
+        await message.answer("Усе чудово! А в тебе?")
+    elif text == "Анекдот":
+        await message.answer("Приходить вампір до вампірського нічного бару, підійшов до стійки біля бару.Бармен: - Що хочете випити: крові 1-ї, 2-ї, 3-ї групи, може крові тварини Вампір: - Налийте мені кип'яченої води. Усі у барі застигли. Бармен: — Ви захворіли чи вам погано? Вампір: - Та ні! Налийте мені кип'яченої води. У цей час дістає з кишені використану прокладку і каже: — Я сьогодні вирішив побалуватися чайком.")
+    else:
+        await message.answer("Натисни одну з кнопок")
+
 
 
 async def main():
