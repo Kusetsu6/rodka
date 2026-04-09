86c51ea98679db2df6f7a3a0f1e87815123827ef
Lyceum <lukantatarenkov@gmail.com>
2026-04-09 09:03:38 +0300

    rodka

--- bot.py
+++ bot.py
@@ -15,8 +15,23 @@ dp = Dispatcher()
 async def start_handler(message: Message):
     await message.answer("Привіт! Я твій перший бот!")
 
-# Головна функція для запуску бота
+@dp.message(Сommand("start"))
+async def start_command(message: Message):
+    await message.answer("Привіт! я телеграм бот. напиши команду /help, щоб дізнатися, що я вмію!")
+
+@dp.message(Сommand("help"))
+async def help_command(message: Message):
+    help_text = (
+        "Доступні команди:\n"
+        "/start - Привітатися з ботом\n"
+        "/help - Показати цю довідку\n\n"
+        "Я поки буду для тебе не настільки корисним, але може в майбутньому я стану розумніше"
+    )
+    await message.answer(help_text)
+
+
 async def main():
+    print("Бот запущений...")
     await dp.start_polling(bot)
 
 
